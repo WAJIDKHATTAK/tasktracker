279054e7136988123de7685b6a3b0ffa12c2509a
WAJIDKHATTAK <https://www.github.com/WAJIDKHATTAK/>
2023-02-24 20:05:12 +0500

    stupid heading chnaged in app

--- tasktracker/src/App.js
+++ tasktracker/src/App.js
@@ -5,10 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo" />   
+          <h2>Why does react suck so bad</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
